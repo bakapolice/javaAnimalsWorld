135a1171c10220b5b9d6cb33c2769693a5e92018
bakapolice <undergroundbaka@gmail.com>
2022-01-05 15:33:32 +0300

    ClientListener refactored. Changed data validation

--- src/controller/Listener/ClientListener.java
+++ src/controller/Listener/ClientListener.java
@@ -42,161 +42,47 @@ public class ClientListener implements ActionListener, ItemListener {
         });
     }
 
+
     @Override
     public void actionPerformed(ActionEvent e) {
         String message;
+
+        // Обработка события "Создать"
         if (e.getSource() == clientForm.getButtonCreate()) {
             message = log + "Нажата кнопка " + clientForm.getButtonCreate().getActionCommand();
             clientForm.getTextAreaLogs().append(message + '\n');
-            String selected = clientForm.getListWhatToCreate().getSelectedItem();
-            String name = clientForm.getTextFieldName().getText();
-            String weigh = clientForm.getTextFieldWeight().getText();
-
-            if (!selected.isEmpty()) {
-                if (!name.isEmpty()) {
-                    if (!weigh.isEmpty()) {
-                        try {
-                            float fWeigh = Float.parseFloat(weigh);
-//                            switch (clientForm.getListWhatToCreate().getSelectedIndex()){
-//                                case 0 -> GeneralController.createHerbivore(name, fWeigh);
-//                                case 1 -> GeneralController.createPredator(name, fWeigh);
-//                                case 2 -> GeneralController.createGrass(name, fWeigh);
-//                            }
-                        } catch (NumberFormatException ex) {
-                            ex.printStackTrace();
-                            clientForm.getTextAreaLogs().append(error + "Значение поля 'Вес' введено некорректно\n");
-                            
-                            throw new IllegalArgumentException("Значение веса введено некорректно");
-                        }
-                    } else {
-                        clientForm.getTextAreaLogs().append(error + "Заполните поле 'Вес'\n");
-                        return;
-                    }
-                } else {
-                    clientForm.getTextAreaLogs().append(error + "Заполните поле 'Имя'\n");
-                    return;
-                }
-            } else {
-                clientForm.getTextAreaLogs().append(error + "Выберите кого создать!\n");
-                return;
-            }
-            message = log +
-                    "Создать: " + selected + "; " +
-                    "Имя: " + name + "; " +
-                    "Вес: " + weigh;
-            clientForm.getTextAreaLogs().append(message + '\n');
+            createPerform();
         }
 
 
+        // Обработка события "Убить"
         if (e.getSource() == clientForm.getButtonKill()) {
             message = log + "Нажата кнопка " + clientForm.getButtonKill().getActionCommand();
             clientForm.getTextAreaLogs().append(message + '\n');
-
-            String selected = clientForm.getChoiceAllAliveAnimals().getSelectedItem();
-            if (selected.isEmpty()) {
-                clientForm.getTextAreaLogs().append(error + "Выберите кого убить!\n");
-                return;
-            }
-
-            message = log + "Убить: " + selected;
-            clientForm.getTextAreaLogs().append(message + '\n');
+            killPerform();
         }
+
+        // Обработка события "Покормить"
         if (e.getSource() == clientForm.getButtonFeed()) {
             message = log + "Нажата кнопка " + clientForm.getButtonFeed().getActionCommand();
             clientForm.getTextAreaLogs().append(message + '\n');
-
-            if (clientForm.getCbgFeed().getSelectedCheckbox() != null) {
-                if (clientForm.getCbHerbivores().getState()) {
-                    String selectedHerbivore = clientForm.getListAllAliveHerbivoresToFeed().getSelectedItem();
-                    String selectedFood = clientForm.getChoiceAllFood().getSelectedItem();
-                    if (!selectedHerbivore.isEmpty()) {
-                        if (!selectedFood.isEmpty()) {
-                            message = log +
-                                    "Покормить травоядное " + selectedHerbivore +
-                                    "; Еда: " + selectedFood;
-                            clientForm.getTextAreaLogs().append(message + '\n');
-                        } else clientForm.getTextAreaLogs().append(error + "Выберите чем кормить!\n");
-                    } else clientForm.getTextAreaLogs().append(error + "Выберите кого кормить!\n");
-                }
-                if (clientForm.getCbPredators().getState()) {
-                    String selectedPredator = clientForm.getListAllAlivePredators().getSelectedItem();
-                    String selectedFood = clientForm.getChoiceAllAliveHerbivores().getSelectedItem();
-                    if (!selectedPredator.isEmpty()) {
-                        if (!selectedFood.isEmpty()) {
-                            message = log +
-                                    "Покормить хищника " + selectedPredator +
-                                    "; Еда: " + selectedFood;
-                            clientForm.getTextAreaLogs().append(message + '\n');
-                        } else clientForm.getTextAreaLogs().append(error + "Выберите чем кормить!\n");
-                    } else clientForm.getTextAreaLogs().append(error + "Выберите кого кормить!\n");
-                }
-            } else clientForm.getTextAreaLogs().append(error + "Выберите кого покормить!\n");
+            feedPerform();
         }
+
+        // Обработка события "Вывести на экран"
         if (e.getSource() == clientForm.getButtonPrint()) {
             message = log + "Нажата кнопка " + clientForm.getButtonPrint().getActionCommand();
             clientForm.getTextAreaLogs().append(message + '\n');
-            if(clientForm.getCbgPrint().getSelectedCheckbox() == null)
-            {
-                clientForm.getTextAreaLogs().append(error + "Выберите что вывести на экран!\n");
-                return;
-            }
-            if (clientForm.getCbAllAnimals().getState()) {
-                clientForm.getTextAreaPrint().append("Вывод на экран списка всех животных\n");
-            }
-            if (clientForm.getCbAllAliveAnimals().getState()) {
-                clientForm.getTextAreaPrint().append("Вывод на экран списка всех живых животных\n");
-            }
-            if (clientForm.getCbAllHerbivores().getState()) {
-                clientForm.getTextAreaPrint().append("Вывод на экран списка всех травоядных\n");
-            }
-            if (clientForm.getCbAllPredators().getState()) {
-                clientForm.getTextAreaPrint().append("Вывод на экран списка всех хищников\n");
-            }
-            if (clientForm.getCbAllAliveHerbivores().getState()) {
-                clientForm.getTextAreaPrint().append("Вывод на экран списка всех живых травоядных\n");
-            }
-            if (clientForm.getCbAllAlivePredators().getState()) {
-                clientForm.getTextAreaPrint().append("Вывод на экран списка всех живых хищников\n");
-            }
-            if (clientForm.getCbAllFood().getState()) {
-                clientForm.getTextAreaPrint().append("Вывод на экран списка всей еды\n");
-            }
+            printPerform();
         }
 
+        // Обработка события "Начать работу"
         if (e.getSource() == clientForm.getButtonStart()) {
             message = log + "Нажата кнопка " + clientForm.getButtonStart().getActionCommand();
             clientForm.getTextAreaLogs().append(message + '\n');
-            String host = clientForm.getTextFieldHost().getText();
-            String sPort = clientForm.getTextFieldPort().getText();
-            int port;
-            if(!host.isEmpty())
-            {
-                if(!sPort.isEmpty()){
-                    try{
-                        port = Integer.parseInt(sPort);
-                    }
-                    catch (NumberFormatException ex){
-                        ex.printStackTrace();
-                        clientForm.getTextAreaLogs().append(error + "Значение порта введено некорректно!\n");
-                        throw new IllegalArgumentException("Значение порта введено некорректно");
-                    }
-                }
-                else {
-                    clientForm.getTextAreaLogs().append(error + "Введите значение порта!\n");
-                    return;
-                }
-            }
-            else
-            {
-                clientForm.getTextAreaLogs().append(error + "Введите адрес хоста!\n");
-                return;
-            }
-            message = log +
-                    "Подключение к серверу... " +
-                    host + ":" + port;
-            clientForm.enableComponents();
-            clientForm.getTextAreaLogs().append(message + '\n');
+            startPerform();
         }
+
         System.out.println(e.getSource());
         System.out.println(e.getActionCommand());
     }
@@ -229,5 +115,153 @@ public class ClientListener implements ActionListener, ItemListener {
             }
         }
         System.out.println(e.getItem());
+    }
+
+    private void createPerform() {
+        String message;
+        String selected = clientForm.getListWhatToCreate().getSelectedItem();
+        String name = clientForm.getTextFieldName().getText();
+        String weigh = clientForm.getTextFieldWeight().getText();
+
+        if (selected.isEmpty()) {
+            clientForm.getTextAreaLogs().append(error + "Выберите кого создать!\n");
+            return;
+        }
+        if (name.isEmpty()) {
+            clientForm.getTextAreaLogs().append(error + "Заполните поле 'Имя'\n");
+            return;
+        }
+        if (weigh.isEmpty()) {
+            clientForm.getTextAreaLogs().append(error + "Заполните поле 'Вес'\n");
+            return;
+        }
+
+        try {
+            float fWeigh = Float.parseFloat(weigh);
+//                            switch (clientForm.getListWhatToCreate().getSelectedIndex()){
+//                                case 0 -> GeneralController.createHerbivore(name, fWeigh);
+//                                case 1 -> GeneralController.createPredator(name, fWeigh);
+//                                case 2 -> GeneralController.createGrass(name, fWeigh);
+//                            }
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+            clientForm.getTextAreaLogs().append(error + "Значение поля 'Вес' введено некорректно\n");
+        }
+        message = log +
+                "Создать: " + selected + "; " +
+                "Имя: " + name + "; " +
+                "Вес: " + weigh;
+        clientForm.getTextAreaLogs().append(message + '\n');
+    }
+
+
+    private void killPerform() {
+        String message;
+        String selected = clientForm.getChoiceAllAliveAnimals().getSelectedItem();
+        if (selected.isEmpty()) {
+            clientForm.getTextAreaLogs().append(error + "Выберите кого убить!\n");
+            return;
+        }
+        message = log + "Убить: " + selected;
+        clientForm.getTextAreaLogs().append(message + '\n');
+    }
+
+    private void feedPerform() {
+        String message;
+        if (clientForm.getCbgFeed().getSelectedCheckbox() == null) {
+            clientForm.getTextAreaLogs().append(error + "Выберите кого покормить!\n");
+            return;
+        }
+
+        if (clientForm.getCbHerbivores().getState()) {
+            String selectedHerbivore = clientForm.getListAllAliveHerbivoresToFeed().getSelectedItem();
+            String selectedFood = clientForm.getChoiceAllFood().getSelectedItem();
+            if (selectedHerbivore.isEmpty()) {
+                clientForm.getTextAreaLogs().append(error + "Выберите кого кормить!\n");
+                return;
+            }
+            if (selectedFood.isEmpty()) {
+                clientForm.getTextAreaLogs().append(error + "Выберите чем кормить!\n");
+                return;
+            }
+            message = log +
+                    "Покормить травоядное " + selectedHerbivore +
+                    "; Еда: " + selectedFood;
+            clientForm.getTextAreaLogs().append(message + '\n');
+        }
+
+        if (clientForm.getCbPredators().getState()) {
+            String selectedPredator = clientForm.getListAllAlivePredators().getSelectedItem();
+            String selectedFood = clientForm.getChoiceAllAliveHerbivores().getSelectedItem();
+            if (selectedPredator.isEmpty()) {
+                clientForm.getTextAreaLogs().append(error + "Выберите чем кормить!\n");
+                return;
+            }
+            if (selectedFood.isEmpty()) {
+                clientForm.getTextAreaLogs().append(error + "Выберите кого кормить!\n");
+                return;
+            }
+            message = log +
+                    "Покормить хищника " + selectedPredator +
+                    "; Еда: " + selectedFood;
+            clientForm.getTextAreaLogs().append(message + '\n');
+        }
+    }
+
+    private void printPerform() {
+        if (clientForm.getCbgPrint().getSelectedCheckbox() == null) {
+            clientForm.getTextAreaLogs().append(error + "Выберите что вывести на экран!\n");
+            return;
+        }
+        if (clientForm.getCbAllAnimals().getState()) {
+            clientForm.getTextAreaPrint().append("Вывод на экран списка всех животных\n");
+        }
+        if (clientForm.getCbAllAliveAnimals().getState()) {
+            clientForm.getTextAreaPrint().append("Вывод на экран списка всех живых животных\n");
+        }
+        if (clientForm.getCbAllHerbivores().getState()) {
+            clientForm.getTextAreaPrint().append("Вывод на экран списка всех травоядных\n");
+        }
+        if (clientForm.getCbAllPredators().getState()) {
+            clientForm.getTextAreaPrint().append("Вывод на экран списка всех хищников\n");
+        }
+        if (clientForm.getCbAllAliveHerbivores().getState()) {
+            clientForm.getTextAreaPrint().append("Вывод на экран списка всех живых травоядных\n");
+        }
+        if (clientForm.getCbAllAlivePredators().getState()) {
+            clientForm.getTextAreaPrint().append("Вывод на экран списка всех живых хищников\n");
+        }
+        if (clientForm.getCbAllFood().getState()) {
+            clientForm.getTextAreaPrint().append("Вывод на экран списка всей еды\n");
+        }
+    }
+
+
+    private void startPerform() {
+        String message;
+        String host = clientForm.getTextFieldHost().getText();
+        String sPort = clientForm.getTextFieldPort().getText();
+        int port = 0;
+        if (host.isEmpty()) {
+            clientForm.getTextAreaLogs().append(error + "Введите адрес хоста!\n");
+            return;
+        }
+        if (sPort.isEmpty()) {
+            clientForm.getTextAreaLogs().append(error + "Введите значение порта!\n");
+            return;
+        }
+
+        try {
+            port = Integer.parseInt(sPort);
+        } catch (NumberFormatException ex) {
+            ex.printStackTrace();
+            clientForm.getTextAreaLogs().append(error + "Значение порта введено некорректно!\n");
+        }
+
+        message = log +
+                "Подключение к серверу... " +
+                host + ":" + port;
+        clientForm.enableComponents();
+        clientForm.getTextAreaLogs().append(message + '\n');
     }
 }
